dcaf2e7b87ccca54251e14a4087d89c4f111f53d
isuruperera <izu9493trgz>
2018-01-21 20:07:49 +0530

    Added empty class for game weight calculation

---
+++ src/main/java/com/uom/cse/ValianzScoreBoardBackEnd/Logic/GameWeight.java
@@ -0,0 +1,10 @@
+package com.uom.cse.ValianzScoreBoardBackEnd.Logic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameWeight {
+    public void calculate(int teamID,int gameID, float score){
+
+    }
+}
